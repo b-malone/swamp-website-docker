7266ebd28a6c294927f35a33554411e47debb2cc
Bailey Malone <bmalone@pndlm.com>
2020-01-08 10:03:18 -0800

    Fixing IDs for users and roles.

--- database/seeds/UserRolesTable.php
+++ database/seeds/UserRolesTable.php
@@ -4,21 +4,21 @@ use Illuminate\Database\Seeder;
 
 class UserRolesTable extends Seeder
 {
-		/**
-		 * Run the database seeds.
-		 *
-		 * @return void
-		 */
-		public function run()
-		{
-				DB::table('user_roles')->insert([
-					'user_id' => 3,
-					'role_id' => 1
-				]);
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('user_roles')->insert([
+			'user_id' => 3,
+			'role_id' => 1
+		]);
 
-				DB::table('user_roles')->insert([
-					'user_id' => 4,
-					'role_id' => 1
-				]);
-		}
+		DB::table('user_roles')->insert([
+			'user_id' => 4,
+			'role_id' => 1
+		]);
+	}
 }
