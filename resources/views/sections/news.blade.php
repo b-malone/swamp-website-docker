f59e5dbae0625e1ecb0219fc8e1db5c87ee94f3c
Bailey Malone <bmalone@pndlm.com>
2020-01-08 11:03:24 -0800

    Breaking up sections into separate templates

---
+++ resources/views/sections/news.blade.php
@@ -0,0 +1,41 @@
+<div class="theme-heading">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-6 col-md-offset-3">
+				<h3>news</h3>
+				<div class="dd-theme-border">
+					<span class="theme-icon"><i class="fa fa-tint" aria-hidden="true"></i></span>
+				</div>
+				<p>SWAMP is a community and volunteer run organization.
+					Our activities help monitor and improve the health of wetlands in
+					Washington state.
+				</p>
+				<p>
+					Peter Ritson, PhD., an environmental scientist,
+					is looking for volunteers to search for amphibians and their eggs.
+					This is the tenth year of this entirely volunteer program which monitors
+					Clark County's wetlands by examining amphibian populations. The project
+					involves training community volunteers to search for pond breeding
+					amphibians and their eggs this winter, then reporting the findings to
+					local and state agencies.
+				</p>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<div class="bolg-section">
+	<div class="container">
+		<div class="row">
+
+			<!-- <div class="col-md-6 col-sm-12 col-xs-12 marB30">
+				@foreach ($posts as $post)
+					<p>POST</p>
+				@endforeach
+			</div> -->
+		
+		</div>
+	</div>
+</div>
